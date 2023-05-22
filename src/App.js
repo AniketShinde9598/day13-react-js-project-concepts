081cdc9caeac34ad339099b5bcf2f3392d4921b9
AniketShinde9598 <aniketshinde9598@gmail.com>
2023-05-22 15:05:59 +0530

    title hello done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Title from "./components/Title";
+
 function App() {
-  return <h1 className="bg-primary text-light p-3">Hello World</h1>;
+  return (
+    <div>
+      <Title />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+function Title() {
+  return (
+    <div>
+      <h1 className="bg-success text-light p-3">Hello World</h1>
+    </div>
+  );
+}
+
+export default Title;
